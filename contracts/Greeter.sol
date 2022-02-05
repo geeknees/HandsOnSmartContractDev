aac072f5480b38d111beaab6ff3631d84dc7828a
Masumi Kawasaki <geeknees@gmail.com>
2022-02-05 22:10:13 +0900

    add owner()

--- contracts/Greeter.sol
+++ contracts/Greeter.sol
@@ -1,7 +1,13 @@
+// SPDX-License-Identifier: UNLICENSED
 pragma solidity >= 0.4.0 < 0.8.12;
 
 contract Greeter {
   string private _greeting = "Hello, World!";
+  address private _owner;
+
+  constructor() {
+    _owner = msg.sender;
+  }
 
   function greet() external view returns(string memory) {
     return _greeting;
@@ -9,5 +15,9 @@ contract Greeter {
 
   function setGreeting(string calldata greeting) external {
     _greeting = greeting;
+  }
+
+  function owner() public view returns(address) {
+    return _owner;
   }
 }
--- test/greeter_test.js
+++ test/greeter_test.js
@@ -1,9 +1,9 @@
 const GreeterContract = artifacts.require("Greeter");
 
-contract("Greeter", () => {
+contract("Greeter", (accounts) => {
   it("has been deployd successfully", async () => {
     const greeter = await GreeterContract.deployed();
-    assert(greeter, "Contract was not deplyed");
+    assert(greeter, "contract failed to deploy");
   });
 
   describe("greet()", () => {
@@ -12,6 +12,21 @@ contract("Greeter", () => {
       const expected = "Hello, World!";
       const actual = await greeter.greet();
       assert.equal(actual, expected, "greeted with Hello, World('");
+    });
+  });
+
+  describe("owner()", () => {
+    it("returns the address of the owner", async () => {
+      const greeter = await GreeterContract.deployed();
+      const owner = await greeter.owner();
+      assert(owner, "the current owner");
+    });
+
+    it("matches the address that originally deployed the contract", async () => {
+      const greeter = await GreeterContract.deployed();
+      const owner = await greeter.owner();
+      const expected = accounts[0];
+      assert.equal(owner, expected, "matches address used to deploy contract");
     });
   });
 });
