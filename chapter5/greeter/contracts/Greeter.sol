ebe3365c69f003acd90bc83aaeb461af3efcead0
Masumi Kawasaki <geeknees@gmail.com>
2022-02-05 22:48:25 +0900

    add chapter5

---
+++ chapter5/greeter/contracts/Greeter.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity >= 0.4.0 < 0.8.12;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Greeter is Ownable {
+  string private _greeting = "Hello, World!";
+
+  function greet() external view returns(string memory) {
+    return _greeting;
+  }
+
+  function setGreeting(string calldata greeting) external onlyOwner {
+    _greeting = greeting;
+  }
+}
